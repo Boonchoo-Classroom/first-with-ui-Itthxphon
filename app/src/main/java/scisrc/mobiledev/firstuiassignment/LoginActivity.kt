abe40fac8c7ce094c53f21ad0fa52bf2d5659df6
itthiphon <itthiphon.p@ku.th>
2025-01-13 18:59:48 +0700

    Itthiphon Phooseelit 6530202960

--- app/src/main/java/scisrc/mobiledev/firstuiassignment/LoginActivity.kt
+++ app/src/main/java/scisrc/mobiledev/firstuiassignment/LoginActivity.kt
@@ -1,16 +1,17 @@
 package scisrc.mobiledev.firstuiassignment
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-
 class LoginActivity : AppCompatActivity() {
 
     lateinit var backToMainBtn: Button
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
